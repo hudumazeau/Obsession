3608e2e5fcfed410d4b8f7d715a56e07c78a537e
hugo.dumazeau@etu.udamail.fr <hugo.dumazeau@etu.udamail.fr>
2016-08-18 22:39:00 +0200

    cron

---
+++ cron.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hudumazeau
+ * Date: 18/08/16
+ * Time: 22:14
+ */
+$urlMiniature='img/affiches/miniature/';
+$urlHD='img/affiches/HD/';
+$urlWeb='web/bundles/obsessionmain/';
+$urlSrc='src/ObsessionMainBundle/Ressources/public/';
+$affichesWeb=scandir($urlWeb.$urlHD);
+$affichesSrc=scandir($urlSrc.$urlHD);
+if(count($affichesSrc)!=count($affichesWeb)){
+    foreach ($affichesWeb as $afficheWeb){
+        if(!in_array($afficheWeb,$affichesSrc)){
+            copy($urlWeb.$urlHD.$afficheWeb,$urlSrc.$urlHD.$afficheWeb);
+            copy($urlWeb.$urlMiniature.$afficheWeb,$urlSrc.$urlMiniature.$afficheWeb);
+        }
+    }
+    foreach ($affichesSrc as $afficheSrc){
+        if(!in_array($afficheSrc,$affichesWeb)){
+            unlink($urlSrc.$urlMiniature.$afficheSrc);
+            unlink($urlSrc.$urlHD.$afficheSrc);
+        }
+    }
+}
+?>
